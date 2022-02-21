6280f74737c87cd7d19af5566836b5f15e9f788d
devgarage <dhianabli@outlook.com>
2022-02-21 11:28:45 +0100

    add update and delete buttons

--- core/routes.py
+++ core/routes.py
@@ -13,21 +13,27 @@ def user_records():
     #username = request.args.get("user")
     #email = request.args.get("email")
     if request.method == "POST":
-        username = request.form['user']
-        email = request.form['email']
 
-        if username and email:
-            existing_user = User.query.filter(
-                User.username == username or User.email == email
-            ).first()
-            if existing_user:
-                return make_response(f"{username} ({email}) already created!")
-            new_user = User(username=username, email=email, created=dt.now(),
-                bio="my cool bio",
-                admin=False,
-            )  # Create an instance of the User class
-            db.session.add(new_user)  # Adds new User record to database
-            db.session.commit()  # Commits all changes
-            redirect(url_for("user_records"))
+        if request.form['delete'] is not None:
+            h_user = request.form['h_user']
+            User.query.filter_by(username=h_user).delete()
+            db.session.commit()
+        else:
+            username = request.form['user']
+            email = request.form['email']
+
+            if username and email:
+                existing_user = User.query.filter(
+                    User.username == username or User.email == email
+                ).first()
+                if existing_user:
+                    return make_response(f"{username} ({email}) already created!")
+                new_user = User(username=username, email=email, created=dt.now(),
+                    bio="my cool bio",
+                    admin=False,
+                )  # Create an instance of the User class
+                db.session.add(new_user)  # Adds new User record to database
+                db.session.commit()  # Commits all changes
+                redirect(url_for("user_records"))
 
     return render_template("users.html", users=User.query.all(), title="Show Users")
